8d9a582b0ae6658199986610fc439299ac181daf
liambaker <liam.baker412@gmail.com>
2013-10-09 21:25:13 +0100

    Initial Commit of Dataservice
    Signed-off-by: liambaker <liam.baker412@gmail.com>

---
+++ SBBTDataservice/AlienAbductionGameService.vb
@@ -0,0 +1,18 @@
+﻿Public Class AlienAbductionGameService
+    Implements IAlienAbductionGameService
+
+    Public Function GetData(ByVal value As Integer) As String Implements IAlienAbductionGameService.GetData
+        Return String.Format("You entered: {0}", value)
+    End Function
+
+    Public Function GetDataUsingDataContract(ByVal composite As CompositeType) As CompositeType Implements IAlienAbductionGameService.GetDataUsingDataContract
+        If composite Is Nothing Then
+            Throw New ArgumentNullException("composite")
+        End If
+        If composite.BoolValue Then
+            composite.StringValue &= "Suffix"
+        End If
+        Return composite
+    End Function
+
+End Class
